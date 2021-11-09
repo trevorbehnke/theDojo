5d0afcf306831a0985e98ce185c1d967b40bf6cb
trevor <me@trevorbehnke.com>
2021-11-09 10:28:34 -0800

    feat: :sparkles: hooks, reducers, pages and routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Dashboard from "./pages/dashboard/Dashboard";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+import Create from "./pages/create/Create";
+import Project from "./pages/project/Project";
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <div className="container">
+          <Switch>
+            <Route exact path="/">
+              <Dashboard />
+            </Route>
+            <Route exact path="/create">
+              <Create />
+            </Route>
+            <Route exact path="/projects/:id">
+              <Project />
+            </Route>
+            <Route exact path="/login">
+              <Login />
+            </Route>
+            <Route exact path="/signup">
+              <Signup />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;
